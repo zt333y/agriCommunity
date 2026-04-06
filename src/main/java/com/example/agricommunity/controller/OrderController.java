414d1dabe913cfa2ed541b97550821f343054aa1
nanfei <zt33y3@gmail.com>
2026-04-06 17:06:15 +0800

    1

--- src/main/java/com/example/agricommunity/controller/OrderController.java
+++ src/main/java/com/example/agricommunity/controller/OrderController.java
@@ -6,7 +6,7 @@ import com.example.agricommunity.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
